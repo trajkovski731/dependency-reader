79926848633843670c06f0369f964bc6812202d2
filip.trajkovski <filip.trajkovski@digitalnatives.mk>
2019-12-09 12:16:45 +0100

    Plugin and HelloController added.

--- src/main/java/com/north47/dependencyreader/HelloController.java
+++ src/main/java/com/north47/dependencyreader/HelloController.java
@@ -1,7 +1,9 @@
 package com.north47.dependencyreader;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class HelloController {
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello ELena! You are not so loud";
+    }
+
+    @PostMapping(value = "/create", consumes = "multipart/form-data")
+    public String createDependencyFile(MultipartFile file) {
+        //file.transferTo();
+        System.out.println("This is the file:" + file.getName());
+        return "We are ok";
     }
 }
