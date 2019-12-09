2e90e922a65d56ce0afffd818a0559ed554aba06
filip.trajkovski <filip.trajkovski@digitalnatives.mk>
2019-12-09 12:42:19 +0100

    Plugin and HelloController added.

--- src/main/java/com/north47/dependencyreader/HelloController.java
+++ src/main/java/com/north47/dependencyreader/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class HelloController {
 
@@ -14,9 +16,11 @@ public class HelloController {
 
 
     @PostMapping(value = "/create", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public String importUsers(@RequestParam("files") MultipartFile file) {
+    public String importUsers(@RequestParam("files") MultipartFile file) throws IOException {
         //file.transferTo();
         System.out.println("This is the file:" + file.getName());
+        String content = new String(file.getBytes(), "UTF-8");
+        System.out.println(content);
         return "We are ok";
     }
 }
