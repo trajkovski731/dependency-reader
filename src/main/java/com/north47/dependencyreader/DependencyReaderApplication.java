4314986f0018e10db4a5207c3f917481709c5919
C803602 <elena.stojanovska@sanitas.com>
2019-12-10 13:00:29 +0100

    fix

--- src/main/java/com/north47/dependencyreader/DependencyReaderApplication.java
+++ src/main/java/com/north47/dependencyreader/DependencyReaderApplication.java
@@ -2,19 +2,14 @@ package com.north47.dependencyreader;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableAdminServer
 public class DependencyReaderApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(DependencyReaderApplication.class)
-                .web(WebApplicationType.REACTIVE)
-                .run(args);
-        ;
+        SpringApplication.run(DependencyReaderApplication.class, args);
     }
 
 }
